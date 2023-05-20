d5a908657b4d8ee0ada4929a0252f00f9ff70d11
omeymaa <omeyma.maatoug@gmail.com>
2023-05-20 20:33:26 +0200

    show infos user

--- my-profile.php
+++ my-profile.php
@@ -2,6 +2,7 @@
 require('./templates/header.php');
 require('./templates/navbar.php');
 require('./lib/session-start.php');
+require('./actions/users/myProfileAction.php');
 ?>
 
 <section class="container py-5">
@@ -22,58 +23,68 @@ require('./lib/session-start.php');
 
         <div class="col-9 col-lg-9">
             <h1 class="fw-bold purple">Mes informations personnelles</h1>
+            <?php
+            if (isset($error)) {
+                echo '<p>' . $error . '</p>';
+            }
 
-            <form method="POST" id="signup-form" class="py-4">
-                <div class="row">
-                    <div class="col-md-6 mb-4">
-                        <div class="form-outline">
-                            <span class="form-label" for="firstname"></span>
-                            <input type="text" id="firstname" class="form-control form-control-sm" placeholder="Prénom" name="firstname" />
+            // if (isset($getHisQuestions)) { 
+            if ($infosUser = $getInfosUser->fetch()) { ?>
+
+                <form method="POST" id="signup-form" class="py-4">
+                    <fieldset disabled>
+                        <div class="row">
+                            <div class="col-md-6 mb-4">
+                                <div class="form-outline">
+                                    <span class="form-label" for="firstname">Prénom</span>
+                                    <input type="text" id="firstname" class="form-control form-control-sm" placeholder="<?= $infosUser['firstname']; ?>" name="firstname" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <div class="form-outline">
+                                    <span class="form-label" for="lastname">Nom</span>
+                                    <input type="text" id="lastname" class="form-control form-control-sm" placeholder="<?= $infosUser['lastname']; ?>" name="lastname" />
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 mb-4">
-                        <div class="form-outline">
-                            <span class="form-label" for="lastname"></span>
-                            <input type="text" id="lastname" class="form-control form-control-sm" placeholder="Nom" name="lastname" />
+
+                        <div class="form-outline mb-4">
+                            <span class="form-label" for="email">E-mail</span>
+                            <input type="email" id="email" class="form-control form-control-sm" placeholder="<?= $infosUser['email']; ?>" name="email" />
                         </div>
-                    </div>
-                </div>
 
-                <div class="form-outline mb-4">
-                    <span class="form-label" for="email"></span>
-                    <input type="email" id="email" class="form-control form-control-sm" placeholder="E-mail" name="email" />
-                </div>
-
-                <div class="form-outline mb-4">
-                    <span class="form-label" for="password"></span>
-                    <input type="password" id="password" class="form-control form-control-sm" placeholder="Mot de passe" name="password" />
-                </div>
-
-                <div class="form-outline mb-4">
-                    <span class="form-label" for="confirm_password"></span>
-                    <input type="password" id="confirm_password" name="confirm_password" class="form-control form-control-sm" placeholder="Confirmez le mot de passe" />
-                </div>
-
-                <div class="row">
-                    <div class="col-md-6 mb-4">
-                        <div class="form-outline">
-                            <span class="form-label" for="guest"></span>
-                            <input type="number" id="guest" class="form-control form-control-sm" placeholder="Nombre de convives par défaut" name="guest" />
+                        <div class="form-outline mb-4">
+                            <span class="form-label" for="password">Mot de passe</span>
+                            <input type="password" id="password" class="form-control form-control-sm" placeholder="<?= $infosUser['password']; ?>" name="password" />
                         </div>
-                    </div>
-                    <div class="col-md-6 mb-4">
-                        <div class="form-outline">
-                            <span class="form-label" for="allergens"></span>
-                            <input type="text" id="allergens" class="form-control form-control-sm" placeholder="Allergie alimentaire" name="allergens" />
+
+                        <div class="form-outline mb-4">
+                            <span class="form-label" for="confirm_password">Confirmez le mot de passe</span>
+                            <input type="password" id="confirm_password" name="confirm_password" class="form-control form-control-sm" placeholder="<?= $infosUser['password']; ?>" />
                         </div>
+
+                        <div class="row">
+                            <div class="col-md-6 mb-4">
+                                <div class="form-outline">
+                                    <span class="form-label" for="guest">Nombre de convives par défaut</span>
+                                    <input type="number" id="guest" class="form-control form-control-sm" placeholder="<?= $infosUser['guest']; ?>" name="guest" />
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <div class="form-outline">
+                                    <span class="form-label" for="allergens">Allergie alimentaire</span>
+                                    <input type="text" id="allergens" class="form-control form-control-sm" placeholder="<?= $infosUser['allergens']; ?>" name="allergens" />
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <div class="pt-1 mb-4">
+                        <a href="edit-profile.php?id=<?= $infosUser['id']; ?>" class="orange-button btn">Modifier mes informations</a>
                     </div>
-                </div>
 
-                <div class="pt-1 mb-4">
-                    <button class="btn orange-button" type="submit" name="validate">Modifier mes informations</button>
-                </div>
-
-            </form>
+                </form>
+            <?php } ?>
         </div>
 
     </div>
