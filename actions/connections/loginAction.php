90927a674a22a77394a3baddff99ea99ab715f86
omeymaa <omeyma.maatoug@gmail.com>
2023-05-20 17:29:12 +0200

    add script js confirm password

---
+++ actions/connections/loginAction.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require('./lib/config.php');
+
+if (isset($_POST['validate'])) {
+
+    // vérifier si l'utilisateur a bien vérifié tous les champs
+    if (!empty($_POST['email']) and !empty($_POST['password'])) {
+
+        // données de l'utilisateur
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+
+        // verifier si l'utilisateur existe (et que le pseudo est correct)
+        $checkIfUserExists = $db->prepare('SELECT * FROM user WHERE email = ?');
+        $checkIfUserExists->execute(array($email));
+
+        if ($checkIfUserExists->rowCount() > 0) {
+
+            // recuperer les donnees de l'utilisateur
+            $usersInfos = $checkIfUserExists->fetch();
+
+            // verifier si le mot de passe est correct
+            if (password_verify($password, $usersInfos['password'])) {
+
+                // authentifier l'utilisateur sur le site et recuperer ses donnes dans des variables globales
+                $_SESSION['auth'] = true;
+                $_SESSION['id'] = $usersInfos['id'];
+                $_SESSION['email'] = $usersInfos['email'];
+                $_SESSION['firstname'] = $usersInfos['firstname'];
+                $_SESSION['lastname'] = $usersInfos['lastname'];
+
+                // rediriger l'utilisateur vers la page d'accueil
+                header('location:index.php');
+            } else {
+                $error = 'Votre pseudo ou mot de passe est incorrect';
+            }
+        } else {
+            $error = 'Votre pseudo ou mot de passe est incorrect';
+        }
+    } else {
+        $error = 'Veuillez remplir tous les champs';
+    }
+}
--- signup.php
+++ signup.php
@@ -14,7 +14,7 @@ require('actions/connections/signupAction.php');
                         </div>
                         <div class="col-md-6 col-lg-7 d-flex align-items-center">
                             <div class="card-body p-4 p-lg-5">
-                                <form method="POST">
+                                <form method="POST" id="signup-form" onsubmit="return CheckPassword();">
                                     <?php
                                     if (isset($error)) {
                                         echo "<p class='alert alert-danger'>$error</p>";
@@ -28,56 +28,57 @@ require('actions/connections/signupAction.php');
                                     <div class="row">
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <span class="form-label" for="form3Example1m"></span>
-                                                <input type="text" id="form3Example1m" class="form-control form-control-sm" placeholder="Prénom" name="firstname" required />
+                                                <span class="form-label" for="firstname"></span>
+                                                <input type="text" id="firstname" class="form-control form-control-sm" placeholder="Prénom" name="firstname" required />
                                             </div>
                                         </div>
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <span class="form-label" for="form3Example1n"></span>
-                                                <input type="text" id="form3Example1n" class="form-control form-control-sm" placeholder="Nom" name="lastname" required />
+                                                <span class="form-label" for="lastname"></span>
+                                                <input type="text" id="lastname" class="form-control form-control-sm" placeholder="Nom" name="lastname" required />
                                             </div>
                                         </div>
                                     </div>
 
                                     <div class="form-outline mb-4">
-                                        <span class="form-label" for="form2Example17"></span>
-                                        <input type="email" id="form2Example17" class="form-control form-control-sm" placeholder="E-mail" name="email" required />
+                                        <span class="form-label" for="email"></span>
+                                        <input type="email" id="email" class="form-control form-control-sm" placeholder="E-mail" name="email" required />
                                     </div>
 
                                     <div class="form-outline mb-4">
-                                        <span class="form-label" for="form2Example27"></span>
-                                        <input type="password" id="form2Example27" class="form-control form-control-sm" placeholder="Mot de passe" name="password" required />
+                                        <span class="form-label" for="password"></span>
+                                        <input type="password" id="password" class="form-control form-control-sm" placeholder="Mot de passe" name="password" required />
                                     </div>
 
                                     <div class="form-outline mb-4">
-                                        <span class="form-label" for="form2Example37"></span>
-                                        <input type="text" id="form2Example37" class="form-control form-control-sm" placeholder="Confirmez le mot de passe" />
+                                        <span class="form-label" for="confirm_password"></span>
+                                        <input type="password" id="confirm_password" name="confirm_password" class="form-control form-control-sm" placeholder="Confirmez le mot de passe" required />
                                     </div>
 
                                     <div class="row">
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <span class="form-label" for="form3Example1m"></span>
-                                                <input type="number" id="form3Example1m" class="form-control form-control-sm" placeholder="Nombre de convives par défaut" name="guest" />
+                                                <span class="form-label" for="guest"></span>
+                                                <input type="number" id="guest" class="form-control form-control-sm" placeholder="Nombre de convives par défaut" name="guest" />
                                             </div>
                                         </div>
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <span class="form-label" for="form3Example1n"></span>
-                                                <input type="text" id="form3Example1n" class="form-control form-control-sm" placeholder="Allergie alimentaire" name="allergens" />
+                                                <span class="form-label" for="allergens"></span>
+                                                <input type="text" id="allergens" class="form-control form-control-sm" placeholder="Allergie alimentaire" name="allergens" />
                                             </div>
                                         </div>
                                     </div>
 
                                     <div class="form-check">
-                                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-                                        <label class="form-check-label" for="form2Example3">
+                                        <input class="form-check-input me-2" type="checkbox" value="" id="check" />
+                                        <label class="form-check-label" for="check">
                                             <p class="small">J'accepte toutes les <a href="#!" class="purple">conditions générales</a></p>
                                         </label>
                                     </div>
 
                                     <div class="pt-1 mb-4">
+                                        <p id="message"></p>
                                         <button class="btn orange-button btn-lg" type="submit" name="validate">S'inscrire</button>
                                     </div>
                                     <p class="small">Vous avez déjà un compte ? <a href="login.php" class="purple">Connexion</a></p>
@@ -93,5 +94,31 @@ require('actions/connections/signupAction.php');
             </div>
         </div>
     </div>
-
 </section>
+
+<script>
+    function CheckPassword() {
+        let password = document.getElementById('password').value;
+        let confirm_password = document.getElementById('confirm_password').value;
+        let message = document.getElementById('message');
+        let checkbox = document.getElementById('check');
+
+        if (password.length !== 0) {
+            if (password === confirm_password) {
+                if (checkbox.checked) {
+                    //message.textContent = 'Les mots de passe correspondent.';
+                    return true; // Permettre la soumission du formulaire
+                } else {
+                    message.textContent = 'Veuillez accepter les conditions générales.';
+                    return false; // Empêcher la soumission du formulaire
+                }
+            } else {
+                message.textContent = 'Les mots de passe ne correspondent pas.';
+                return false; // Empêcher la soumission du formulaire
+            }
+        } else {
+            message.textContent = 'Veuillez saisir un mot de passe.';
+            return false; // Empêcher la soumission du formulaire
+        }
+    }
+</script>
